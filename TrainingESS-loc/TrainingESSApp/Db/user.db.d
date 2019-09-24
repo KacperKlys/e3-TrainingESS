1f8138736a33ca1e956f5533c2c65cc8ff822fe5
vagrant <vagrant@localhost.localdomain>
2019-09-24 14:49:39 +0200

    v2

---
+++ TrainingESS-loc/TrainingESSApp/Db/user.db.d
@@ -0,0 +1,3 @@
+TrainingESSApp/Db/user.db: TrainingESSApp/Db/circle.db \
+ TrainingESSApp/Db/dbExample1.db \
+ TrainingESSApp/Db/dbExample2.db
---
+++ cmds/TrainingESS.cmd
@@ -0,0 +1,4 @@
+require TrainingESS, master
+
+
+IocInit()
